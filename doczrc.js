c1e9f46ae09b455ea80cbd5f365d7a91e465e3eb
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-09-25 12:13:41 +0530

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ import * as colors from './src/utils/colors'
 
 export default {
   title: 'Hobby Land',
-  ignore: ['**/blog/**', 'README.md'],
+  ignore: ['**/blog/**', 'README.md', 'CODE_OF_CONDUCT.md', 'CONTRIBUTING.md'],
   menu: ['Getting Started', 'Components'],
   themeConfig: {
     colors: {
